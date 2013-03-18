df54548bf4f89ca1a4993aee5edc4875cb029441
ryzhovau@gmail.com <ryzhovau@gmail.com@2ec4c832-8166-dbf9-75c5-1c02bdf2e45f>
2013-03-18 13:33:19 +0000

    [chroot] start script: add some fool-proof

--- chroot-install/wedro_chroot.sh
+++ chroot-install/wedro_chroot.sh
@@ -4,14 +4,13 @@ SCRIPT_NAME=$(basename $0)
 SCRIPT_START='99'
 SCRIPT_STOP='01'
 
-MOUNT_DIR='/DataVolume/shares'
-
+MOUNT_DIR="/DataVolume/shares"
 CHROOT_DIR="__CHROOT_DIR_PLACEHOLDER__"
 CHROOT_SERVICES="$(cat $CHROOT_DIR/chroot-services.list)"
 
 ### BEGIN INIT INFO
 # Provides:          $SCRIPT_NAME
-# Required-Start:    wedro_mount.sh
+# Required-Start:
 # Required-Stop:
 # X-Start-Before:
 # Default-Start:     2 3 4 5
@@ -31,29 +30,18 @@ script_remove() {
 
 #######################################################################
 
-MOUNT_COUNTS="$(mount | grep $CHROOT_DIR | wc -l)"
-
-check_mounted() {
-  if [[ $MOUNT_COUNTS -lt 1 ]]; then
-      echo "CHROOT seems to be unmounted, exiting..."
-      exit 1
-  fi
-}
-
-CHROOT_COUNTS="$(chroot $CHROOT_DIR mount | wc -l)"
+shareDirMountCount="$(mount | grep "$CHROOT_DIR" | wc -l)"
 
 check_started() {
-  check_mounted
-  if [[ $CHROOT_COUNTS -gt 0 ]]; then
-      echo "CHROOT seems to be started, exiting..."
+  if [[ $shareDirMountCount -gt 0 ]]; then
+      echo "CHROOT servicess seems to be already started, exiting..."
       exit 1
   fi
 }
 
 check_stopped() {
-  check_mounted
-  if [[ $CHROOT_COUNTS -eq 0 ]]; then
-      echo "CHROOT seems to be stopped, exiting..."
+  if [[ $shareDirMountCount -eq 0 ]]; then
+      echo "CHROOT services seems to be already stopped, exiting..."
       exit 1
   fi
 }
@@ -61,30 +49,24 @@ check_stopped() {
 #######################################################################
 
 start() {
-#    check_started
-
+    check_started
     mount --bind $MOUNT_DIR $CHROOT_DIR/mnt
-
     chroot $CHROOT_DIR mount -t proc none /proc -o rw,noexec,nosuid,nodev
     chroot $CHROOT_DIR mount -t sysfs none /sys -o rw,noexec,nosuid,nodev
     chroot $CHROOT_DIR mount -t devpts none /dev/pts -o rw,noexec,nosuid,gid=5,mode=620
-
     for ITEM in $CHROOT_SERVICES; do
         chroot $CHROOT_DIR service $ITEM start
     done
 }
 
 stop() {
-#    check_stopped
-
+    check_stopped
     for ITEM in $CHROOT_SERVICES; do
         chroot $CHROOT_DIR service $ITEM stop
     done
-
     chroot $CHROOT_DIR umount /dev/pts
     chroot $CHROOT_DIR umount /sys
     chroot $CHROOT_DIR umount /proc
-
     umount $CHROOT_DIR/mnt
 }
 
@@ -116,7 +98,7 @@ case "$1" in
         script_remove
     ;;
     *)
-        echo $"Usage: $0 {start|stop|restart|update|upgrade|upgrade-system}"
+        echo $"Usage: $0 {start|stop|restart|install|init|remove}"
         exit 1
 esac
 
