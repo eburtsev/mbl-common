55092bd22088b826d7100cd5ad37fbf21969f300
alexey.suhinin@gmail.com <alexey.suhinin@gmail.com@2ec4c832-8166-dbf9-75c5-1c02bdf2e45f>
2013-12-11 09:15:56 +0000

    [chroot] Fix mounting pseudo filesystems

--- chroot-install/wedro_chroot.sh
+++ chroot-install/wedro_chroot.sh
@@ -51,9 +51,10 @@ check_stopped() {
 start() {
     check_started
     mount --bind $MOUNT_DIR $CHROOT_DIR/mnt
-    chroot $CHROOT_DIR mount -t proc none /proc -o rw,noexec,nosuid,nodev
     chroot $CHROOT_DIR mount -t sysfs none /sys -o rw,noexec,nosuid,nodev
-    chroot $CHROOT_DIR mount -t devpts none /dev/pts -o rw,noexec,nosuid,gid=5,mode=620
+    mount -o bind /dev $CHROOT_DIR/dev
+    mount -o bind /dev/pts $CHROOT_DIR/dev/pts
+    mount -o bind /proc $CHROOT_DIR/proc
     for ITEM in $CHROOT_SERVICES; do
         chroot $CHROOT_DIR service $ITEM start
     done
@@ -64,9 +65,10 @@ stop() {
     for ITEM in $CHROOT_SERVICES; do
         chroot $CHROOT_DIR service $ITEM stop
     done
-    chroot $CHROOT_DIR umount /dev/pts
+    umount $CHROOT_DIR/proc
+    umount $CHROOT_DIR/dev/pts
+    umount $CHROOT_DIR/dev
     chroot $CHROOT_DIR umount /sys
-    chroot $CHROOT_DIR umount /proc
     umount $CHROOT_DIR/mnt
 }
 
